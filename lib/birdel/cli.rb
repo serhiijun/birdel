f365c5158d1cd0e7b86c1e2ae794540710ae68fd
Serhii <serhii.jun@gmail.com>
2023-05-04 19:03:48 +0300

    bla

--- lib/birdel/cli.rb
+++ lib/birdel/cli.rb
@@ -1,10 +1,19 @@
 require 'thor'
+require_relative "birdel/synth/synth_actor"
+require_relative "birdel/com/com_actor"
+require_relative "birdel/map/map_actor"
+require_relative "birdel/component/component_actor"
 
 module Birdel
   class CLI < Thor
     desc "gcom NAME", "Generate a new component"
     def gcom(name)
       Birdel::Com.roll(name)
+    end
+
+    desc "gcom NAME", "Generate a new component"
+    def gent(name)
+      Birdel::Map.roll(name)
     end
 
     desc "synth", "Syncronize components assets"
