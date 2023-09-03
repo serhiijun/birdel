3ce8b6d754f7c8b8b32e4b64d9274c90a453797f
Serhii <serhii.jun@gmail.com>
2023-09-03 18:59:15 +0300

    Fix BaseActor require

--- lib/birdel/base/base_actor.rb
+++ lib/birdel/base/base_actor.rb
@@ -1,8 +1,10 @@
 module Birdel
-  class BaseActor
-    attr_reader :current_user
-    def initialize(current_user:)
-      @current_user = current_user
+  module Base
+    class BaseActor
+      attr_reader :current_user
+      def initialize(current_user:)
+        @current_user = current_user
+      end
     end
   end
 end
